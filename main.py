b87263f1458cbfd6abbc2b2dd6bb7931764e0305
Efrem-Yohanis <efremyohanis116@gmail.com>
2026-07-24 12:36:27 -0700

    test

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"name": "efrem", "age": 15}
+    return {"name": "efrem", "age": 15, "message": "Hello, welcome to my FastAPI application!"}
